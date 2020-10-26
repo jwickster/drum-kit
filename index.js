57308f66d396558d97ab18d0cc04fd0addf39421
Jwickster <joshwickster@gmail.com>
2020-10-26 09:51:15 -0600

    edits

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ var numButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        //alert("I got clicked");
 
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+
+
 
     });
 
 }
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
